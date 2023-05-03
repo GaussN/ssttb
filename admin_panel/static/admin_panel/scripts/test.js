b6f35a04c9e6fbf23f61a38beae2ad100c098565
Goga <barbaradob17@gmail.com>
2023-05-03 23:59:25 +0300

    script for adding tests

--- admin_panel/static/admin_panel/scripts/test.js
+++ admin_panel/static/admin_panel/scripts/test.js
@@ -1,49 +1,48 @@
 $('document').ready(() => {
-		let answer_template = '<div class="answer form-group form-inline" >'+
-			'<label class="control-label">Answer:</label>'+
-			'<input type="text" class="form-control" name="answer_text">'+
-			'<label>Right:</label>'+
-			'<input type="checkbox" class="form-checkbox" name="is_right">'+
-			'<button type="button" class="btn btn-danger remove-answer"><span class="glyphicon glyphicon-trash"></span></button>'+
-		'</div>';
-		let question_template = '<div class="panel question">'+
-			'<div class="form-group">'+
-				'<label>Question:</label>'+
-				'<input type="text" class="form-control" name="question_text">'+
-				'<label>Multianswer:</label>'+
-				'<input type="checkbox" class="form-checkbox" name="is_multianswer">'+
-			'</div>'+
-			'<div class="answers">'+
-			'</div>'+
-			'<button type="button" class="btn btn-danger del-question">Del question</button>'+
-			'<button type="button" class="btn btn-success add-answer" >Add answer</button><br>'+
-		'</div>';
-		function set_handlers(){
-			$("button").off();
-			$("button.remove-answer").click((event)=>{
-				$(event.target).closest("div.answer").remove();
-			});
-			$("button.add-answer").click((event)=>{
-				$(event.target).parent().find("div.answers").append(answer_template);
-				set_handlers();
-			});
-			$("button.del-question").click((event)=>{
-				console.log($(event.target).parent());
-				$(event.target).parent().remove();
-			});
-			$("button.add-question").click((event)=>{
-				$("div.questions").append(question_template);
-				set_handlers();
-			});
-
-			$("button[type=submit]").click((event)=>{
+	let answer_template = '<div class="answer form-group form-inline" >'+
+		'<label class="control-label">Answer:</label>'+
+		'<input type="text" class="form-control" name="answer_text">'+
+		'<label>Right:</label>'+
+		'<input type="checkbox" class="form-checkbox" name="is_right">'+
+		'<button type="button" class="btn btn-danger remove-answer"><span class="glyphicon glyphicon-trash"></span></button>'+
+	'</div>';
+	let question_template = '<div class="panel question">'+
+		'<div class="form-group">'+
+			'<label>Question:</label>'+
+			'<input type="text" class="form-control" name="question_text">'+
+			'<label>Multianswer:</label>'+
+			'<input type="checkbox" class="form-checkbox" name="is_multianswer">'+
+		'</div>'+
+		'<div class="answers">'+
+		'</div>'+
+		'<button type="button" class="btn btn-danger del-question">Del question</button>'+
+		'<button type="button" class="btn btn-success add-answer" >Add answer</button><br>'+
+	'</div>';
+	function set_handlers(){
+		$("button").off();
+		$("button.remove-answer").click((event)=>{
+			$(event.target).closest("div.answer").remove();
+		});
+		$("button.add-answer").click((event)=>{
+			$(event.target).parent().find("div.answers").append(answer_template);
+			set_handlers();
+		});
+		$("button.del-question").click((event)=>{
+			console.log($(event.target).parent());
+			$(event.target).parent().remove();
+		});
+		$("button.add-question").click((event)=>{
+			$("div.questions").append(question_template);
+			set_handlers();
+		});
+		$("button.submit").click((event)=>{
 			event.preventDefault();
 			let testObj = {
 				"title": $("input[name=test_title]").val(),
 				"visible": $("input[name=test_visible]").prop("checked"),
 				"questions": []
 			};
-
+	
 			let questions = $("div.question");
 			for(let question of questions){
 				let questionObj = {
@@ -59,12 +58,23 @@ $('document').ready(() => {
 					};
 					questionObj["answers"].push(asnwerObj);
 				}
-
 				testObj["questions"].push(questionObj);
 			}
-
-			console.log(testObj);
+			//console.log(testObj);
+		   	$.ajax({
+				url: window.location.href,
+				method: "POST",
+				data: JSON.stringify(testObj),
+				headers: {
+					"X-CSRFToken": [$("input[name=csrfmiddlewaretoken]").val()]
+				}
+			}).fail(() => {
+				alert("fail");
+			});
+			//fetch(window.location.href)
 		});
-		}
-		set_handlers();
-	});
+	}
+	set_handlers();
+});
+
+
